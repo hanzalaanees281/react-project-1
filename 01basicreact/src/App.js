3695331f1a24b3a6c83fff7765c3a5f3f9f78a30
hanzalaanees281 <hanzalaanees555@gmail.com>
2023-12-29 00:12:15 +0500

    react setup with create-react-app and the second is vite react use

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1>HELLO | Hanzala Anees</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+ 
+
+  return (
+   <h1>HELLO | Hanzala Anees</h1>
+  )
+}
+
+export default App
